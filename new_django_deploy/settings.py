6ea2140ae59c40f0999dbabbe1fd8de82e950475
sanam-tamang <snm.tmg7@gmail.com>
2023-02-08 14:54:10 +0545

    added debug false

--- new_django_deploy/settings.py
+++ new_django_deploy/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)j(no8o*^d30$badc^-^=9%_-5(27k$5nw905i*+bwr12j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','djangonewdeploy-production.up.railway.app']
 
